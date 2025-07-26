6b47419f82fdcd63af1104f0468e1fe64348b93d
NimsaraVTA <cisco1998learn@gmail.com>
2025-07-26 18:39:16 +0530

    Commit to local repo

---
+++ src/main/java/net/codejava/pahana_edu_billing_system/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package net.codejava.pahana_edu_billing_system;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
---
+++ src/main/java/net/codejava/pahana_edu_billing_system/resources/JavaEE8Resource.java
@@ -0,0 +1,20 @@
+package net.codejava.pahana_edu_billing_system.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("javaee8")
+public class JavaEE8Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping")
+                .build();
+    }
+}
